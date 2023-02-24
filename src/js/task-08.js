fe911164fb1659d583ac5bd43a0ae2621436e2fd
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-24 13:17:46 +0200

    7, 8, 9

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,16 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', event => {
+  event.preventDefault(); 
+
+  const { email, password } = form.elements; 
+  const userData = { email: email.value, password: password.value }; 
+
+  if (!userData.email || !userData.password) {
+    alert('Please fill in all fields'); 
+    return;
+  }
+
+  console.log(userData); 
+  form.reset(); 
+});
