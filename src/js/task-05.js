cd0eb643944cdf50f06e5e2c9b9b8420bed837c7
olegbezpalko74 <sasha_ermakova@ukr.net>
2023-02-25 12:38:18 +0200

    update task

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -3,7 +3,7 @@ const nameOutputEl = document.querySelector('#name-output');
 
 nameInputEl.addEventListener('input', () => {
   if (nameInputEl.value.trim() !== '') {
-    nameOutputEl.textContent = nameInputEl.value;
+    nameOutputEl.textContent = nameInputEl.value.trim();
   } else {
     nameOutputEl.textContent = 'Anonymous';
   }
